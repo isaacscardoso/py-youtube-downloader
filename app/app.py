547d2fb039d8a8f0f4f8603fab5d4821171de9ce
isaacscardoso <isaacscardosoblues@gmail.com>
2024-08-23 13:36:13 -0300

    feat: Adiciona a funcionalidade para baixar em formato de música mp3. Adiciona opções de qualidade para vídeo e música.

--- app/app.py
+++ app/app.py
@@ -1,7 +1,16 @@
 import threading
-import tkinter as tk
-from tkinter import filedialog, messagebox
 from downloader import YoutubeVideoDownloader
+from tkinter import (
+    messagebox,
+    filedialog,
+    Tk,
+    Label,
+    Entry,
+    Button,
+    Radiobutton,
+    StringVar,
+    OptionMenu,
+)
 
 
 class YoutubeDownloaderApp:
@@ -9,39 +18,101 @@ class YoutubeDownloaderApp:
         self.root = root
         self.root.title("Downloader de Vídeos do YouTube")
 
+        self.download_format = StringVar(value="mp4")
+        self.music_quality = StringVar(value="192")  # Default quality set to 192 kbps
+        self.video_quality = StringVar(value="best")  # Default quality set to best
+
         self._create_widgets()
 
     def _create_widgets(self):
-        tk.Label(self.root, text="URL do Vídeo do YouTube:").grid(
+        Label(self.root, text="URL do Vídeo do YouTube:").grid(
             row=0, column=0, padx=10, pady=5
         )
-        tk.Label(self.root, text="Caminho de Saída:").grid(
+        Label(self.root, text="Caminho de Saída:").grid(
             row=1, column=0, padx=10, pady=5
         )
 
-        self.url_entry = tk.Entry(self.root, width=50)
+        self.url_entry = Entry(self.root, width=50)
         self.url_entry.grid(row=0, column=1, padx=10, pady=5)
 
-        self.output_entry = tk.Entry(self.root, width=50)
+        self.output_entry = Entry(self.root, width=50)
         self.output_entry.grid(row=1, column=1, padx=10, pady=5)
 
-        browse_button = tk.Button(
+        browse_button = Button(
             self.root, text="Procurar", command=self._browse_output_path
         )
         browse_button.grid(row=1, column=2, padx=10, pady=5)
 
-        self.status_label = tk.Label(self.root, text="Status: Aguardando...")
-        self.status_label.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
+        self.status_label = Label(self.root, text="Status: Aguardando...")
+        self.status_label.grid(row=7, column=0, columnspan=3, padx=10, pady=10)
 
-        download_button = tk.Button(
+        download_button = Button(
             self.root, text="Baixar", command=self._start_download_thread
         )
-        download_button.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
+        download_button.grid(row=8, column=0, columnspan=3, padx=10, pady=10)
+
+        # Radio buttons to select format
+        Radiobutton(
+            self.root,
+            text="Vídeo (MP4)",
+            variable=self.download_format,
+            value="mp4",
+            command=self._toggle_quality_options,
+        ).grid(row=2, column=0, padx=10, pady=5)
+        Radiobutton(
+            self.root,
+            text="Música (MP3)",
+            variable=self.download_format,
+            value="mp3",
+            command=self._toggle_quality_options,
+        ).grid(row=2, column=1, padx=10, pady=5)
+
+        # OptionMenu to select music quality (initially hidden)
+        self.quality_label_music = Label(self.root, text="Qualidade de Música:")
+        music_quality_options = ["128", "192", "320"]
+        self.quality_menu_music = OptionMenu(
+            self.root, self.music_quality, *music_quality_options
+        )
+
+        # OptionMenu to select video quality (initially visible)
+        self.quality_label_video = Label(self.root, text="Qualidade de Vídeo:")
+        video_quality_options = [
+            "2160p60",
+            "1440p60",
+            "1080p60",
+            "720p60",
+            "480p",
+            "360p",
+            "240p",
+            "144p",
+            "best",
+        ]
+        self.quality_menu_video = OptionMenu(
+            self.root, self.video_quality, *video_quality_options
+        )
+
+        # Display video quality options by default
+        self.quality_label_video.grid(row=3, column=0, padx=10, pady=5)
+        self.quality_menu_video.grid(row=3, column=1, padx=10, pady=5)
+
+    def _toggle_quality_options(self):
+        if self.download_format.get() == "mp3":
+            self.quality_label_video.grid_forget()
+            self.quality_menu_video.grid_forget()
+
+            self.quality_label_music.grid(row=3, column=0, padx=10, pady=5)
+            self.quality_menu_music.grid(row=3, column=1, padx=10, pady=5)
+        else:
+            self.quality_label_music.grid_forget()
+            self.quality_menu_music.grid_forget()
+
+            self.quality_label_video.grid(row=3, column=0, padx=10, pady=5)
+            self.quality_menu_video.grid(row=3, column=1, padx=10, pady=5)
 
     def _browse_output_path(self):
         folder_selected = filedialog.askdirectory()
         if folder_selected:
-            self.output_entry.delete(0, tk.END)
+            self.output_entry.delete(0, "end")
             self.output_entry.insert(0, folder_selected)
 
     def _start_download_thread(self):
@@ -52,7 +123,12 @@ class YoutubeDownloaderApp:
             messagebox.showerror("Erro", "Por favor, insira a URL do vídeo.")
             return
 
-        downloader = YoutubeVideoDownloader(self.status_label)
+        downloader = YoutubeVideoDownloader(
+            self.status_label,
+            self.download_format.get(),
+            self.music_quality.get(),
+            self.video_quality.get(),
+        )
         download_thread = threading.Thread(
             target=downloader.download_video, args=(url, output_path)
         )
@@ -60,6 +136,6 @@ class YoutubeDownloaderApp:
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    root = Tk()
     app = YoutubeDownloaderApp(root)
     root.mainloop()
--- app/downloader.py
+++ app/downloader.py
@@ -4,24 +4,55 @@ from interfaces import VideoDownloaderInterface
 
 
 class YoutubeVideoDownloader(VideoDownloaderInterface):
-    def __init__(self, status_label):
+    def __init__(
+        self,
+        status_label,
+        download_format="mp4",
+        music_quality="192",
+        video_quality="best",
+    ):
         self.status_label = status_label
+        self.download_format = download_format
+        self.music_quality = music_quality
+        self.video_quality = video_quality
 
     def download_video(self, url: str, output_path: str):
-        ydl_opts = {
-            "outtmpl": f"{output_path}/%(title)s.%(ext)s",
-            "format": "bestvideo+bestaudio/best",
-            "merge_output_format": "mp4",
-            "progress_hooks": [self._update_status],
-            "noplaylist": True,
-            "quiet": False,
-        }
+        if self.download_format == "mp4":
+            ydl_opts = {
+                "outtmpl": f"{output_path}/%(title)s.%(ext)s",
+                "format": (
+                    f"bestvideo[height<={self.video_quality}]+bestaudio/best"
+                    if self.video_quality != "best"
+                    else "bestvideo+bestaudio/best"
+                ),
+                "merge_output_format": "mp4",
+                "progress_hooks": [self._update_status],
+                "noplaylist": True,
+                "quiet": False,
+            }
+        else:
+            ydl_opts = {
+                "outtmpl": f"{output_path}/%(title)s.%(ext)s",
+                "format": "bestaudio",
+                "postprocessors": [
+                    {
+                        "key": "FFmpegExtractAudio",
+                        "preferredcodec": "mp3",
+                        "preferredquality": self.music_quality,
+                    }
+                ],
+                "progress_hooks": [self._update_status],
+                "noplaylist": True,
+                "quiet": False,
+            }
 
         try:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                self._set_status(f"Baixando vídeo de {url}...")
+                self._set_status(f"Baixando {self.download_format} de {url}...")
                 ydl.download([url])
-            self._set_status(f"Vídeo baixado com sucesso para {output_path}")
+            self._set_status(
+                f"{self.download_format.upper()} baixado com sucesso para {output_path}"
+            )
         except Exception as e:
             self._set_status(f"Erro: {e}")
             messagebox.showerror("Erro", f"Ocorreu um erro: {e}")
